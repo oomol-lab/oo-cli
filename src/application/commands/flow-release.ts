9549b45df4a3bee546366e84e2b73185d189eda0
l1shen <648952316@qq.com>
2026-09-04 17:45:05 +0800

    feat(flow): update Open Flow command to beta.10

--- src/application/commands/flow-release.ts
+++ src/application/commands/flow-release.ts
@@ -12,12 +12,12 @@ export interface OpenFlowCommandRelease {
 
 export const openFlowCommandRelease = {
     archive: {
-        digest: "75ce924019c532831fa1d6a07bbda1bdebf38f09f46e9091f1ea74635180e74f",
-        length: 104_947,
-        url: "https://static.oomol.com/release/apps/open-flow/command/open-flow-0.1.0-beta.6-75ce924019c532831fa1d6a07bbda1bdebf38f09f46e9091f1ea74635180e74f.tar.gz",
+        digest: "ae93f759f075be4f4177c2da59ca03c09878f68fb3fc5242d05d9262b44c33b0",
+        length: 105_407,
+        url: "https://static.oomol.com/release/apps/open-flow/command/open-flow-0.1.0-beta.10-ae93f759f075be4f4177c2da59ca03c09878f68fb3fc5242d05d9262b44c33b0.tar.gz",
     },
     bunVersion: "1.4.0",
     format: "open-flow-command-release",
-    openFlowVersion: "0.1.0-beta.6",
+    openFlowVersion: "0.1.0-beta.10",
     version: 1,
 } as const satisfies OpenFlowCommandRelease;
